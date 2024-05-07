54f6a1152ead0c9c7f39c00d4cdb9e080de958dc
Abdelaziz Bennacer <a.bennacer@compethance.fr>
2024-05-07 07:03:38 +0200

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Abdelaziz Bennacer on 07/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
